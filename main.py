7987a4c1466b85c85f6ab40ac05c9a88dfd7f98b
fahmidev013 <admin@fahmidev.com>
2025-07-10 16:02:37 +0700

    add main

---
+++ main.py
@@ -0,0 +1,29 @@
+from backend import *
+from frontend.utils import *
+
+
+# Load dataset
+root_directory = os.getcwd()
+customers_data = pd.read_csv(f"{root_directory}/data/customer_data.csv")
+
+@app.route('/customers', methods=['GET'])
+def get_customers():
+    return jsonify(customers_data.to_dict(orient="records"))
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "<h3>This is test</h3>"
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    user_input = data.get("message", "")
+    openaikey = OPENAI_API_KEY
+    
+    
+    if not user_input:
+        return jsonify({"error": "No input provided"}), 400
+    
+    response = conversation.predict(input=user_input)
+    return jsonify({"response": response})
+
