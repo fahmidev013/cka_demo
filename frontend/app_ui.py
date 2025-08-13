60af9c920968fa602b94a122be36a0eb98086f59
fahmidev013 <admin@fahmidev.com>
2025-08-13 12:39:44 +0700

    add memory info on sidebar

--- frontend/app_ui.py
+++ frontend/app_ui.py
@@ -4,6 +4,14 @@ from packages import *
 st.sidebar.title("CKA Artificial Intelligence")
 menu = st.sidebar.radio("List Application", ["Chatbot", "Web Scrapping", "Information Extractor", "Data Science", "Face Recognition"])
 
+# -------------------------
+# 4. Info Monitoring
+# -------------------------
+import psutil, os
+process = psutil.Process(os.getpid())
+mem_usage = process.memory_info().rss / (1024 ** 2)
+st.sidebar.write(f"**Penggunaan Memori:** {mem_usage:.2f} MB")
+
 # Routing halaman berdasarkan menu yang dipilih
 if menu == "Chatbot":
     chatbotPage()
